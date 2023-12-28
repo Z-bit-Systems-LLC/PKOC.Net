600e92c4c00b456c2c3c7a774dc33a93bfb4b604
Jonathan Horvath <qubit@z-bitco.com>
2023-12-28 12:57:07 -0500

    Add a validate signature method

---
+++ src/PKOC.Net/PKOC.Net/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace PKOC.Net
+{
+    internal static class Utilities
+    {
+        internal static bool ValidateSignature(ReadOnlySpan<byte> data, ReadOnlySpan<byte> publicKey, ReadOnlySpan<byte> signature)
+        {
+            using (var algorithm = ECDsa.Create(new ECParameters
+                   {
+                       Curve = ECCurve.NamedCurves.nistP256,
+                       Q =
+                       {
+                           X = publicKey.Slice(1, 32).ToArray(),
+                           Y = publicKey.Slice(33).ToArray()
+                       }
+                   }))
+            {
+#if NETSTANDARD2_0
+                return algorithm.VerifyData(data.ToArray(), signature.ToArray(), HashAlgorithmName.SHA256);
+#else
+                return algorithm.VerifyData(data, signature, HashAlgorithmName.SHA256);
+#endif
+            }
+        }
+    }
+}
---
+++ test/PKOC.Net.Tests/UtilitiesTest.cs
@@ -0,0 +1,24 @@
+﻿using PKOC.Net;
+
+namespace PKOC.Net.Tests;
+
+[TestFixture]
+[TestOf(typeof(Net.Utilities))]
+public class UtilitiesTest
+{
+
+    [Test]
+    public void ValidateSignature()
+    {
+        // Arrange
+        byte[] publicKey = Convert.FromHexString("040ec5d87dc39d14a2c5480686da860c82b16be0b6903b525f84848b79fd463e32bbda1f0252c33503c5287035e6eac55d138d0650dcfb5281d59a9cf4124d2831");
+        byte[] signature = Convert.FromHexString("b98613070c78010b04ed306d143f94ee6dc4eca2585b621405731fb3a53cd877a21685de18435da7cbcc38f1d926300a454efee3594cec5effe28c7feac03d7d");
+        byte[] data = Convert.FromHexString("6fcf5012b224043b09350a4fc5e56a8f");
+
+        // Act
+        bool isSignatureValid = Net.Utilities.ValidateSignature(data, publicKey, signature);
+
+        // Assert
+        Assert.That(isSignatureValid, Is.True);
+    }
+}
