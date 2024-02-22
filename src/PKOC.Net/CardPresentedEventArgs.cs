553f6c42658551aaec06207a60fbc3736ff0a590
Jonathan Horvath <qubit@z-bitco.com>
2024-02-22 15:00:10 -0500

    Add device information in events

--- src/PKOC.Net/CardPresentedEventArgs.cs
+++ src/PKOC.Net/CardPresentedEventArgs.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using PKOC.Net.MessageData;
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 
 namespace PKOC.Net
 {
     public class CardPresentedEventArgs : EventArgs
     {
+        public Guid ConnectionId { get; }
+        
+        public byte Address { get; }
+        
         public CardPresentResponseData CardPresentResponseData { get; }
 
-        public CardPresentedEventArgs(CardPresentResponseData cardPresentResponseData)
+        public CardPresentedEventArgs(Guid connectionId, byte address, CardPresentResponseData cardPresentResponseData)
         {
+            ConnectionId = connectionId;
+            Address = address;
             CardPresentResponseData = cardPresentResponseData;
         }
     }
